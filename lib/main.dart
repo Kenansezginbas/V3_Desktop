6ef209ae210bedec9e7f3b91e57babf0f11f4e29
Kenan Sezginbas <kenansezginbas@hotmail.com>
2022-09-08 15:44:49 +0300

    V3

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'V3 Desktopp',
+      title: 'V3 Desktop',
       home: HomePage(),
     );
   }
